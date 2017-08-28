8ce598875def925842c278d5092bbb5733b3e6d7
古月先生 <907138916@qq.com>
2017-08-28 14:13:54 +0800

    add 快排

--- src/lession_2/js/index.js
+++ src/lession_2/js/index.js
@@ -1,1 +1,28 @@
-console.log('fuck this project ...')
+console.log('fuck this project ...');
+
+
+let mergeSort = (arr) => {
+	let length = arr.length;
+	if (length <= 1) {
+		return arr;
+	}
+	let middle = arr.splice(Math.floor(length / 2), 1);
+	let left = [], right = [], middleValue = middle[0];
+
+	arr.forEach((v, i)=>{
+		if (v < middleValue) {
+			left.push(v);
+		} else {
+			right.push(v);
+		}
+	})
+
+	console.log(left, middle, right);
+
+	return mergeSort(left).concat(middle, mergeSort(right));
+
+}
+
+let sortArr = mergeSort([5,3,1,6,4,0,6,8,7]);
+console.log(sortArr);
+
